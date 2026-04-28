4ce6892adbd8238fe336f78b10253a2d9e3ce329
Your Name <you@example.com>
2026-04-28 08:28:35 -0300

    codigo CartContext completo

--- context/CartContext.tsx
+++ context/CartContext.tsx
@@ -2,6 +2,7 @@
 
 import { Frete } from "@/types/Frete";
 import { Produto } from "@/types/Produto"
+import { createContext, ReactNode, useContext, useState } from "react";
 
 interface CartContextType {
     
@@ -9,4 +10,24 @@ interface CartContextType {
     frete: Frete | null;
     setProduto: (produto: Produto) => void
     setFrete: (frete:Frete) => void
+}
+
+const CartContext = createContext<CartContextType | undefined>(undefined)
+
+//O Provider
+export function CartProvider({children}: {children: ReactNode}){
+    const [produto, setProduto] = useState<Produto | null>(null)
+    const [frete, setFrete] = useState<Frete | null>(null)
+
+    return(
+        <CartContext.Provider value={{produto, frete, setProduto,setFrete}}>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+export const useCart = () => {
+    const context = useContext(CartContext)
+    if(!context) throw new Error("Esqueceu o provider no Layout")
+    return context
 }
